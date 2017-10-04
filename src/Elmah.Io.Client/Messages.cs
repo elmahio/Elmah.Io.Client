987671639ffefcd1c7a6716161a47492b88810c7
Thomas Ardal <thomasardal@gmail.com>
2017-10-04 15:33:04 +0200

    New swagger client to accept status code 413 on message create

--- src/Elmah.Io.Client/Messages.cs
+++ src/Elmah.Io.Client/Messages.cs
@@ -332,7 +332,7 @@ namespace Elmah.Io.Client
             HttpStatusCode _statusCode = _httpResponse.StatusCode;
             cancellationToken.ThrowIfCancellationRequested();
             string _responseContent = null;
-            if ((int)_statusCode != 200 && (int)_statusCode != 201 && (int)_statusCode != 400 && (int)_statusCode != 401 && (int)_statusCode != 403 && (int)_statusCode != 404 && (int)_statusCode != 429)
+            if ((int)_statusCode != 200 && (int)_statusCode != 201 && (int)_statusCode != 400 && (int)_statusCode != 401 && (int)_statusCode != 403 && (int)_statusCode != 404 && (int)_statusCode != 413 && (int)_statusCode != 429)
             {
                 var ex = new HttpOperationException(string.Format("Operation returned an invalid status code '{0}'", _statusCode));
                 if (_httpResponse.Content != null) {
