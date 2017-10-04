3dcb97151acc3105e3c4d1de61c64fba44652387
Thomas Ardal <thomasardal@gmail.com>
2017-10-04 14:12:51 +0200

    Ported to csproj

--- Swagger-Generate.ps1
+++ Swagger-Generate.ps1
@@ -1,3 +1,3 @@
-AutoRest.exe -Namespace Elmah.Io.Client -AddCredentials -CodeGenerator CSharp -OutputDirectory Elmah.Io.Client -Input https://api.elmah.io:443/swagger/docs/v3
+AutoRest.exe -Namespace Elmah.Io.Client -AddCredentials -CodeGenerator CSharp -OutputDirectory src\Elmah.Io.Client -Input https://api.elmah.io:443/swagger/docs/v3
 
 dotnet build Elmah.Io.Client
