c5e0c071affded79c1cfe44264c50e49657c7231
Thomas Ardal <thomasardal@gmail.com>
2020-05-03 15:53:29 +0200

    Decorate data item list with known properties from concrete exceptions

--- src/Elmah.Io.Client/ExceptionExtensions.cs
+++ src/Elmah.Io.Client/ExceptionExtensions.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Elmah.Io.Client.Models;
 
 namespace Elmah.Io.Client
 {
+    /// <summary>
+    /// Extension methods provided by elmah.io for the Exception class.
+    /// </summary>
     public static class ExceptionExtensions
     {
+        /// <summary>
+        /// Pulls as much information as possible from an Exception to a list of elmah.io Items.
+        /// </summary>
         public static List<Item> ToDataList(this Exception exception)
         {
             if (exception == null) return null;
@@ -28,10 +35,82 @@ namespace Elmah.Io.Client
                     result.AddRange(data);
                 }
 
+                if (!string.IsNullOrWhiteSpace(e.HelpLink))
+                {
+                    result.Add(new Item { Key = "Exception.HelpLink", Value = e.HelpLink });
+                }
+
+                var exceptionSpecificItems = ExceptionSpecificItems(e);
+                if (exceptionSpecificItems.Count > 0)
+                {
+                    result.AddRange(exceptionSpecificItems);
+                }
+
                 e = e.InnerException;
             }
 
             return result;
+        }
+
+        /// <summary>
+        /// Helper method to extract items from different known exception types and their properties.
+        /// </summary>
+        private static List<Item> ExceptionSpecificItems(Exception e)
+        {
+            var result = new List<Item>();
+            if (e is ArgumentException ae)
+            {
+                if (!string.IsNullOrWhiteSpace(ae.ParamName))
+                    result.Add(new Item { Key = ItemName(ae, nameof(ae.ParamName)), Value = ae.ParamName });
+            }
+
+            if (e is BadImageFormatException bife)
+            {
+                if (!string.IsNullOrWhiteSpace(bife.FileName))
+                    result.Add(new Item { Key = ItemName(bife, nameof(bife.FileName)), Value = bife.FileName });
+#if NETSTANDARD2_0 || NET45 || NET46 || NET461
+                if (!string.IsNullOrWhiteSpace(bife.FusionLog))
+                    result.Add(new Item { Key = ItemName(bife, nameof(bife.FusionLog)), Value = bife.FusionLog });
+#endif
+            }
+
+            if (e is FileNotFoundException fnfe)
+            {
+                if (!string.IsNullOrWhiteSpace(fnfe.FileName))
+                    result.Add(new Item { Key = ItemName(fnfe, nameof(fnfe.FileName)), Value = fnfe.FileName });
+#if NETSTANDARD2_0 || NET45 || NET46 || NET461
+                if (!string.IsNullOrWhiteSpace(fnfe.FusionLog))
+                    result.Add(new Item { Key = ItemName(fnfe, nameof(fnfe.FusionLog)), Value = fnfe.FusionLog });
+#endif
+            }
+
+#if NETSTANDARD1_4 || NETSTANDARD2_0 || NET45 || NET46 || NET461
+            if (e is System.Net.Sockets.SocketException se)
+            {
+                result.Add(new Item { Key = ItemName(se, nameof(se.SocketErrorCode)), Value = se.SocketErrorCode.ToString() });
+#if NETSTANDARD2_0 || NET45 || NET46 || NET461
+                result.Add(new Item { Key = ItemName(se, nameof(se.ErrorCode)), Value = se.ErrorCode.ToString() });
+#endif
+            }
+#endif
+
+#if NETSTANDARD2_0 || NET45 || NET46 || NET461
+            if (e is System.Net.WebException we)
+            {
+                string ss = ItemName(we, nameof(we.Status));
+                result.Add(new Item { Key = ItemName(we, nameof(we.Status)), Value = we.Status.ToString() });
+            }
+#endif
+
+            return result;
+        }
+
+        /// <summary>
+        /// Generate an item name of a property on an exception.
+        /// </summary>
+        public static string ItemName(Exception exception, string propertyName)
+        {
+            return $"{exception.GetType().Name}.{propertyName}";
         }
 
         private static string Value(IDictionary data, object key)
--- test/Elmah.Io.Client.Test/ExceptionExtensionsTest.cs
+++ test/Elmah.Io.Client.Test/ExceptionExtensionsTest.cs
@@ -1,11 +1,127 @@
 ﻿using NUnit.Framework;
 using System;
+using System.IO;
 using System.Linq;
+using System.Net;
 
 namespace Elmah.Io.Client.Test
 {
     public class ExceptionExtensionsTest
     {
+        [Test]
+        public void CanGenerateDataListWithHelpLink()
+        {
+            // Arrange
+            var exception = new Exception();
+            exception.HelpLink = "http://localhost";
+
+            // Act
+            var result = exception.ToDataList();
+
+            // Assert
+            Assert.That(result.Count, Is.EqualTo(1));
+            Assert.That(result.Any(r => r.Key == "Exception.HelpLink" && r.Value == "http://localhost"));
+        }
+
+        [Test]
+        public void CanGenerateDataListFromArgumentException()
+        {
+            // Arrange
+            var argumentException = new ArgumentException("message", "paramName");
+
+            // Act
+            var result = argumentException.ToDataList();
+
+            // Assert
+            Assert.That(result.Count, Is.EqualTo(1));
+            Assert.That(result.Any(r => r.Key == "ArgumentException.ParamName" && r.Value == "paramName"));
+        }
+
+        [Test]
+        public void CanGenerateDataListFromArgumentNullException()
+        {
+            // Arrange
+            var argumentNullException = new ArgumentNullException("paramName");
+
+            // Act
+            var result = argumentNullException.ToDataList();
+
+            // Assert
+            Assert.That(result.Count, Is.EqualTo(1));
+            Assert.That(result.Any(r => r.Key == "ArgumentNullException.ParamName" && r.Value == "paramName"));
+        }
+
+        [Test]
+        public void CanGenerateDataListFromArgumentOutOfRangeException()
+        {
+            // Arrange
+            var argumentOutOfRangeException = new ArgumentOutOfRangeException("paramName");
+
+            // Act
+            var result = argumentOutOfRangeException.ToDataList();
+
+            // Assert
+            Assert.That(result.Count, Is.EqualTo(1));
+            Assert.That(result.Any(r => r.Key == "ArgumentOutOfRangeException.ParamName" && r.Value == "paramName"));
+        }
+
+        [Test]
+        public void CanGenerateDataListFromSocketException()
+        {
+            // Arrange
+            var socketException = new System.Net.Sockets.SocketException(100);
+
+            // Act
+            var result = socketException.ToDataList();
+
+            // Assert
+            Assert.That(result.Count, Is.EqualTo(2));
+            Assert.That(result.Any(r => r.Key == "SocketException.SocketErrorCode" && r.Value == "100"));
+            Assert.That(result.Any(r => r.Key == "SocketException.ErrorCode" && r.Value == "100"));
+        }
+
+        [Test]
+        public void CanGenerateDataListFromBadImageFormatException()
+        {
+            // Arrange
+            var badImageFormatException = new BadImageFormatException("message", "fileName");
+
+            // Act
+            var result = badImageFormatException.ToDataList();
+
+            // Assert
+            Assert.That(result.Count, Is.EqualTo(1));
+            Assert.That(result.Any(r => r.Key == "BadImageFormatException.FileName" && r.Value == "fileName"));
+        }
+
+        [Test]
+        public void CanGenerateDataListFromFileNotFoundException()
+        {
+            // Arrange
+            var fileNotFoundException = new FileNotFoundException("message", "fileName");
+
+            // Act
+            var result = fileNotFoundException.ToDataList();
+
+            // Assert
+            Assert.That(result.Count, Is.EqualTo(1));
+            Assert.That(result.Any(r => r.Key == "FileNotFoundException.FileName" && r.Value == "fileName"));
+        }
+
+        [Test]
+        public void CanGenerateDataListFromWebException()
+        {
+            // Arrange
+            var webException = new WebException("message", WebExceptionStatus.ProtocolError);
+
+            // Act
+            var result = webException.ToDataList();
+
+            // Assert
+            Assert.That(result.Count, Is.EqualTo(1));
+            Assert.That(result.Any(r => r.Key == "WebException.Status" && r.Value == "ProtocolError"));
+        }
+
         [Test]
         public void CanGenerateDataList()
         {
