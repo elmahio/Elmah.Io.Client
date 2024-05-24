14cb8aa47fe4dc5616b4ce089584835c5d952e3b
Thomas Ardal <thomasardal@gmail.com>
2024-05-24 10:00:48 +0200

    Don't use primary constructor in partial class

--- src/Elmah.Io.Client/Breadcrumb.cs
+++ src/Elmah.Io.Client/Breadcrumb.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿// Don't use primary constructor here since that will cause properties not to be correctly set on partial class
+#pragma warning disable IDE0290 // Use primary constructor
+using System;
 
 namespace Elmah.Io.Client
 {
     /// <summary>
     /// Create a new breadcrumb.
     /// </summary>
-    public partial class Breadcrumb(DateTimeOffset? dateTime = default, string severity = default, string action = default, string message = default)
+    public partial class Breadcrumb
     {
+        /// <summary>
+        /// Create a new breadcrumb.
+        /// </summary>
+        public Breadcrumb(DateTimeOffset? dateTime = default, string severity = default, string action = default, string message = default)
+        {
+            DateTime = dateTime;
+            Severity = severity;
+            Action = action;
+            Message = message;
+        }
     }
 }
+#pragma warning restore IDE0290 // Use primary constructor
