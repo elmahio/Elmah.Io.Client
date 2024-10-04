c86631a4d04b137eebd06aa7cbd042f1cf068d0a
Thomas Ardal <thomasardal@gmail.com>
2024-10-04 12:01:29 +0200

    Generated client from latest changes on api.elmah.io (delete log endpoint)

--- src/Elmah.Io.Client/ElmahioClient.cs
+++ src/Elmah.Io.Client/ElmahioClient.cs
@@ -1079,6 +1079,29 @@ namespace Elmah.Io.Client
         System.Threading.Tasks.Task<Log> GetAsync(string id, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken));
 
         /// <summary>
+        /// Delete a log by its ID.
+        /// </summary>
+        /// <remarks>
+        /// Only empty logs can be deleted. Make sure to delete all messages (including hidden messages) before calling this endpoint.&lt;br/&gt;&lt;br/&gt;Required permission: `logs_delete`
+        /// </remarks>
+        /// <param name="id">The ID of the log to delete.</param>
+        /// <returns>Log was deleted.</returns>
+        /// <exception cref="ElmahIoClientException">A server side error occurred.</exception>
+        void Delete(string id);
+
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <summary>
+        /// Delete a log by its ID.
+        /// </summary>
+        /// <remarks>
+        /// Only empty logs can be deleted. Make sure to delete all messages (including hidden messages) before calling this endpoint.&lt;br/&gt;&lt;br/&gt;Required permission: `logs_delete`
+        /// </remarks>
+        /// <param name="id">The ID of the log to delete.</param>
+        /// <returns>Log was deleted.</returns>
+        /// <exception cref="ElmahIoClientException">A server side error occurred.</exception>
+        System.Threading.Tasks.Task DeleteAsync(string id, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken));
+
+        /// <summary>
         /// Disable a log by its ID.
         /// </summary>
         /// <remarks>
@@ -1493,6 +1516,128 @@ namespace Elmah.Io.Client
                         {
                             string responseText_ = ( response_.Content == null ) ? string.Empty : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
                             throw new ElmahIoClientException("Log not found.", status_, responseText_, headers_, null);
+                        }
+                        else
+                        if (status_ == 429)
+                        {
+                            string responseText_ = ( response_.Content == null ) ? string.Empty : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
+                            throw new ElmahIoClientException("A maximum of 500 requests per minute and 3600 requests per hour are permitted", status_, responseText_, headers_, null);
+                        }
+                        else
+                        {
+                            var responseData_ = response_.Content == null ? null : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
+                            throw new ElmahIoClientException("The HTTP status code of the response was not expected (" + status_ + ").", status_, responseData_, headers_, null);
+                        }
+                    }
+                    finally
+                    {
+                        if (disposeResponse_)
+                            response_.Dispose();
+                    }
+                }
+            }
+            finally
+            {
+                if (disposeClient_)
+                    client_.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Delete a log by its ID.
+        /// </summary>
+        /// <remarks>
+        /// Only empty logs can be deleted. Make sure to delete all messages (including hidden messages) before calling this endpoint.&lt;br/&gt;&lt;br/&gt;Required permission: `logs_delete`
+        /// </remarks>
+        /// <param name="id">The ID of the log to delete.</param>
+        /// <returns>Log was deleted.</returns>
+        /// <exception cref="ElmahIoClientException">A server side error occurred.</exception>
+        public virtual void Delete(string id)
+        {
+            System.Threading.Tasks.Task.Run(async () => await DeleteAsync(id, System.Threading.CancellationToken.None)).GetAwaiter().GetResult();
+        }
+
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <summary>
+        /// Delete a log by its ID.
+        /// </summary>
+        /// <remarks>
+        /// Only empty logs can be deleted. Make sure to delete all messages (including hidden messages) before calling this endpoint.&lt;br/&gt;&lt;br/&gt;Required permission: `logs_delete`
+        /// </remarks>
+        /// <param name="id">The ID of the log to delete.</param>
+        /// <returns>Log was deleted.</returns>
+        /// <exception cref="ElmahIoClientException">A server side error occurred.</exception>
+        public virtual async System.Threading.Tasks.Task DeleteAsync(string id, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken))
+        {
+            if (id == null)
+                throw new System.ArgumentNullException("id");
+
+            var urlBuilder_ = new System.Text.StringBuilder();
+            urlBuilder_.Append(BaseUrl != null ? BaseUrl.TrimEnd('/') : "").Append("/v3/logs/{id}");
+            urlBuilder_.Replace("{id}", System.Uri.EscapeDataString(ConvertToString(id, System.Globalization.CultureInfo.InvariantCulture)));
+
+            var client_ = _httpClient;
+            var disposeClient_ = false;
+            try
+            {
+                using (var request_ = new System.Net.Http.HttpRequestMessage())
+                {
+                    request_.Method = new System.Net.Http.HttpMethod("DELETE");
+
+                    PrepareRequest(client_, request_, urlBuilder_);
+
+                    var url_ = urlBuilder_.ToString();
+                    request_.RequestUri = new System.Uri(url_, System.UriKind.RelativeOrAbsolute);
+
+                    PrepareRequest(client_, request_, url_);
+
+                    var response_ = await client_.SendAsync(request_, System.Net.Http.HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+                    var disposeResponse_ = true;
+                    try
+                    {
+                        var headers_ = System.Linq.Enumerable.ToDictionary(response_.Headers, h_ => h_.Key, h_ => h_.Value);
+                        if (response_.Content != null && response_.Content.Headers != null)
+                        {
+                            foreach (var item_ in response_.Content.Headers)
+                                headers_[item_.Key] = item_.Value;
+                        }
+
+                        ProcessResponse(client_, response_);
+
+                        var status_ = (int)response_.StatusCode;
+                        if (status_ == 204)
+                        {
+                            return;
+                        }
+                        else
+                        if (status_ == 400)
+                        {
+                            string responseText_ = ( response_.Content == null ) ? string.Empty : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
+                            throw new ElmahIoClientException("Id not specified.", status_, responseText_, headers_, null);
+                        }
+                        else
+                        if (status_ == 401)
+                        {
+                            string responseText_ = ( response_.Content == null ) ? string.Empty : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
+                            throw new ElmahIoClientException("API key not valid or no access to resource.", status_, responseText_, headers_, null);
+                        }
+                        else
+                        if (status_ == 402)
+                        {
+                            string responseText_ = ( response_.Content == null ) ? string.Empty : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
+                            throw new ElmahIoClientException("Tried to call the logs API but the trial expired.", status_, responseText_, headers_, null);
+                        }
+                        else
+                        if (status_ == 404)
+                        {
+                            string responseText_ = ( response_.Content == null ) ? string.Empty : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
+                            throw new ElmahIoClientException("Log not found.", status_, responseText_, headers_, null);
+                        }
+                        else
+                        if (status_ == 409)
+                        {
+                            string responseText_ = ( response_.Content == null ) ? string.Empty : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
+                            throw new ElmahIoClientException("Log contains data and cannot be deleted.", status_, responseText_, headers_, null);
                         }
                         else
                         if (status_ == 429)
