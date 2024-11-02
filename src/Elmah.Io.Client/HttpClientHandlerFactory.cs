d4b3d4eb8e5e2749172f63844aae8b2647e2fc43
Thomas Ardal <thomasardal@gmail.com>
2024-11-02 15:51:55 +0100

    Dispose previous instance of HttpClientHandler when renewing

--- src/Elmah.Io.Client/HttpClientHandlerFactory.cs
+++ src/Elmah.Io.Client/HttpClientHandlerFactory.cs
@@ -14,6 +14,8 @@ namespace Elmah.Io.Client
         {
             if (DateTime.Now.Subtract(_initTime) > _lifeTime || _instance == null)
             {
+                TryDispose(_instance);
+
                 if (options.WebProxy != null)
                 {
                     _instance = new HttpClientHandler
@@ -29,9 +31,23 @@ namespace Elmah.Io.Client
                         UseProxy = false,
                     };
                 }
+
                 _initTime = DateTime.Now;
             }
             return _instance;
+        }
+
+        private static void TryDispose(HttpClientHandler instance)
+        {
+            try
+            {
+                instance?.Dispose();
+            }
+            catch
+            {
+                // We have no way of logging any errors during dispose here. There will be a new
+                // instance of HttpClientHandler created in a bit that will replace this object.
+            }
         }
     }
 #pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
