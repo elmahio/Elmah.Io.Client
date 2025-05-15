41198a20358a2c0bc59ae2091444f1ad7bcd08f4
Thomas Ardal <thomasardal@gmail.com>
2025-05-15 10:57:02 +0200

    Added missing json settings for SourceMapsClient

---
+++ src/Elmah.Io.Client/SourceMapsClient.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace Elmah.Io.Client
+{
+    /// <inheritdoc/>
+    public partial class SourceMapsClient
+    {
+        static partial void UpdateJsonSerializerSettings(JsonSerializerSettings settings)
+        {
+            settings.Formatting = Formatting.Indented;
+            settings.DateFormatHandling = DateFormatHandling.IsoDateFormat;
+            settings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
+            settings.NullValueHandling = NullValueHandling.Ignore;
+            settings.ReferenceLoopHandling = ReferenceLoopHandling.Serialize;
+            settings.Converters = [];
+        }
+    }
+}
